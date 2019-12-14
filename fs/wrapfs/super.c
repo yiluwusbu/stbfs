8cbc40118329f4c0ae54accaa60405a53b7e830c
Erez Zadok <ezk@cs.sunysb.edu>
2019-12-14 00:13:51 -0500

    wrapfs: update copyright

--- fs/wrapfs/super.c
+++ fs/wrapfs/super.c
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 1998-2019 Erez Zadok
+ * Copyright (c) 1998-2020 Erez Zadok
  * Copyright (c) 2009	   Shrikar Archak
- * Copyright (c) 2003-2019 Stony Brook University
- * Copyright (c) 2003-2019 The Research Foundation of SUNY
+ * Copyright (c) 2003-2020 Stony Brook University
+ * Copyright (c) 2003-2020 The Research Foundation of SUNY
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
